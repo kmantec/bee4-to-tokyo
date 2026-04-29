b48b251c0506e5b76f4619066ea8c4a425bd26c2
Arnon Kh <59549586+kmantec@users.noreply.github.com>
2026-04-29 17:07:34 +0700

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@
 // See SETUP.md for step-by-step instructions
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAP2FtjsUBMHb-zTsUeIVdsJoviIT4ESTg",
+  authDomain: "bee4-to-tokyo.firebaseapp.com",
+  projectId: "bee4-to-tokyo",
+  storageBucket: "bee4-to-tokyo.firebasestorage.app",
+  messagingSenderId: "636215202563",
+  appId: "1:636215202563:web:4a06b556e002589ce8cf36"
 };
 
 // Set to true to test WITHOUT Firebase (uses LocalStorage only)
-export const OFFLINE_MODE = true;
+export const OFFLINE_MODE = false;
